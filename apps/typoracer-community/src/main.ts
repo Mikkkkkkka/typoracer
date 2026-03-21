0259916427885355e791eaca0aa19c1d83782499
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-22 00:15:18 +0300

    configured nest app to use layouts + rename layouts/layout to layouts/main

--- apps/typoracer-community/src/main.ts
+++ apps/typoracer-community/src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 import { join } from 'path';
+import layouts from 'express-ejs-layouts';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
   app.useStaticAssets(publicPath);
   app.setBaseViewsDir(viewsPath);
   app.setViewEngine('ejs');
+
+  app.set('layout', 'layouts/main');
+  app.use(layouts);
 
   await app.listen(process.env.PORT ?? 3000);
 }
