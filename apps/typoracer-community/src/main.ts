39c4afb306b6c09f1034dfea30d66df630d3796f
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-21 20:17:26 +0300

    configured nestjs to use ejs as the templating engine

--- apps/typoracer-community/src/main.ts
+++ apps/typoracer-community/src/main.ts
@@ -4,21 +4,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 import { join } from 'path';
-import hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const publicPath = join(__dirname, '..', 'public');
-  const viewsPath = join(__dirname, '..', 'views');
+  const viewsPath = join(__dirname, '..', 'views', 'ejs');
 
   app.useStaticAssets(publicPath);
   app.setBaseViewsDir(viewsPath);
-  app.setViewEngine('hbs');
-
-  hbs.registerPartials(join(viewsPath, 'layouts'));
-  hbs.registerPartials(join(viewsPath, 'partials'));
-  hbs.registerHelper('eq', (a: string, b: string) => a === b);
+  app.setViewEngine('ejs');
 
   await app.listen(process.env.PORT ?? 3000);
 }
