0443c3fdf76b8c7bf17cc46cebc4275cbd93f224
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-22 02:30:45 +0300

    code refactoring in main.js

--- apps/typoracer-community/public/js/main.js
+++ apps/typoracer-community/public/js/main.js
@@ -1,19 +1,26 @@
 (function () {
   window.addEventListener('load', function () {
-    const time_load = this.performance.now() / 1000;
-    const load_metrics = this.document.getElementById('load-metrics');
-
-    if (load_metrics) {
-      load_metrics.textContent = `Время загрузки страницы ${time_load} с.`;
-    }
-
-    const nav_links = this.document.querySelectorAll('.nav-bar__link');
-    const current_path = this.document.location.pathname;
-
-    nav_links.forEach((link) => {
-      if (link.pathname === current_path) {
-        link.classList.add('nav-bar__link--active');
-      }
-    });
+    renderLoadMetrics();
+    applyNavbarConditionalStyles();
   });
 })();
+
+function renderLoadMetrics() {
+  const time_load = this.performance.now() / 1000;
+  const load_metrics = this.document.getElementById('load-metrics');
+
+  if (load_metrics) {
+    load_metrics.textContent = `Time to load page: ${time_load} с.`;
+  }
+}
+
+function applyNavbarConditionalStyles() {
+  const nav_links = this.document.querySelectorAll('.nav-bar__link');
+  const current_path = this.document.location.pathname;
+
+  for (let link of nav_links) {
+    if (link.pathname === current_path) {
+      link.classList.add('nav-bar__link--active');
+    }
+  }
+}
