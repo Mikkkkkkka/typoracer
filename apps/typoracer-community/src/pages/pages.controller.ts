da6b2466bf5a331cf3c022830b4d834e875cb4dd
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-22 00:41:36 +0300

    renamed pages names

--- apps/typoracer-community/src/pages/pages.controller.ts
+++ apps/typoracer-community/src/pages/pages.controller.ts
@@ -12,25 +12,29 @@ export class PagesController {
   @Get('play')
   @Render('play')
   getPlay() {
-    return { currentPath: '/play', title: 'Play' };
+    return { currentPath: '/play', title: 'Play Typing Test' };
   }
 
   @Get('leaderboard')
   @Render('leaderboard')
   getLeaderboard() {
-    return { currentPath: '/leaderboard', title: 'Leaderboard' };
+    return { currentPath: '/leaderboard', title: 'Generic Leaderboard' };
   }
 
   @Get('quote-submission')
   @Render('quote-submission')
   getQuoteSubmission() {
-    return { currentPath: '/quote-submission', title: 'Submit a Quote' };
+    return {
+      currentPath: '/quote-submission',
+      title: 'Generic Quote Submission',
+    };
   }
 
   @Get('*')
   getNotFound(@Res() res: express.Response) {
-    return res
-      .status(404)
-      .render('not-found', { currentPath: '', title: 'Page Not Found' });
+    return res.status(404).render('not-found', {
+      currentPath: '',
+      title: 'Generic Page Not Found',
+    });
   }
 }
