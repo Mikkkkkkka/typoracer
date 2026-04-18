a945d36c48376f05e738c925e23d3de3cd54e4de
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-18 05:50:34 +0300

    di: app.module reviring

--- apps/typoracer-community/src/app.module.ts
+++ apps/typoracer-community/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DiscussionsModule } from './discussions/discussions.module';
 import { PagesModule } from './pages/pages.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { QuotesModule } from './quotes/quotes.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [QuotesModule, DiscussionsModule, UsersModule, PagesModule],
+  imports: [
+    PrismaModule,
+    QuotesModule,
+    DiscussionsModule,
+    UsersModule,
+    PagesModule,
+  ],
   controllers: [],
   providers: [],
 })
