a9e99e263d103f46e7cc27fd1e3f91383840c2e2
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-20 19:34:01 +0300

    format: applied prettier formatting and lint auto-fix

--- apps/typoracer-community/src/app.module.ts
+++ apps/typoracer-community/src/app.module.ts
@@ -22,7 +22,7 @@ import { join } from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       path: '/graphql',
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(__dirname, 'schema.gql'),
       sortSchema: true,
       introspection: true,
       playground: false,
