da55747acd613cc353b862b22a84821fde30278a
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-20 17:56:28 +0300

    refactor(di): wired the new services and controllers to di

--- apps/typoracer-community/src/attempts/attempts.module.ts
+++ apps/typoracer-community/src/attempts/attempts.module.ts
@@ -3,10 +3,17 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { QuotesModule } from '../quotes/quotes.module';
 import { AttemptsService } from './attempts.service';
 import { AttemptsController } from './attempts.controller';
+import { QuoteAttemptsController } from './quote-attempts.controller';
+import { UserAttemptsController } from './user-attempts.controller';
 
 @Module({
   imports: [PrismaModule, QuotesModule],
-  controllers: [AttemptsController],
+  controllers: [
+    AttemptsController,
+    QuoteAttemptsController,
+    UserAttemptsController,
+  ],
   providers: [AttemptsService],
+  exports: [AttemptsService],
 })
 export class AttemptsModule {}
--- apps/typoracer-community/src/quotes/quotes.module.ts
+++ apps/typoracer-community/src/quotes/quotes.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { QuoteRecordsEventsService } from './quote-records-events.service';
+import { QuoteRecordsController } from './quote-records.controller';
+import { QuoteRecordsService } from './quote-records.service';
 import { QuotesApiController } from './quotes-api.controller';
 import { QuotesController } from './quotes.controller';
+import { QuotesRecordsEventsService } from './quotes-records-events.service';
 import { QuotesService } from './quotes.service';
 
 @Module({
-  controllers: [QuotesController, QuotesApiController],
-  providers: [QuotesService, QuoteRecordsEventsService],
-  exports: [QuotesService, QuoteRecordsEventsService],
+  controllers: [QuotesApiController, QuotesController, QuoteRecordsController],
+  providers: [QuotesService, QuoteRecordsService, QuotesRecordsEventsService],
+  exports: [QuotesService, QuoteRecordsService, QuotesRecordsEventsService],
 })
 export class QuotesModule {}
