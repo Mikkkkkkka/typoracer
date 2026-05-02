3f914a0922d98ef2b16127562e9b54b9a707b505
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-05-02 14:59:52 +0300

    feat(attempts): wired the updated attempts module to DI

--- apps/typoracer-community/src/attempts/attempts.module.ts
+++ apps/typoracer-community/src/attempts/attempts.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { QuotesModule } from '../quotes/quotes.module';
+import { AttemptsController } from './attempts.controller';
 import { AttemptsApiController } from './attempts-api.controller';
 import { QuoteAttemptsApiController } from './quote-attempts-api.controller';
 import { AttemptsService } from './attempts.service';
 import { UserAttemptsApiController } from './user-attempts-api.controller';
 
 @Module({
-  imports: [PrismaModule, QuotesModule],
+  imports: [PrismaModule, QuotesModule, AuthModule],
   controllers: [
+    AttemptsController,
     AttemptsApiController,
     QuoteAttemptsApiController,
     UserAttemptsApiController,
